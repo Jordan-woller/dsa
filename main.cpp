19e4d9ec24b36370ae3451d91ce76fde181f6a11
jordan-woller <jordan_woller@uri.edu>
2023-03-28 22:57:57 -0400

    adds chrono the time stuff

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 #include <vector>
+#include <chrono>
 
 int main(){
+    auto start = std::chrono::high_resolution_clock::now();
+    //function call here
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+ 
+    std::cout << "Time taken by function: "
+         << duration.count() << " microseconds" << std::endl;
 
     return 0;
 }

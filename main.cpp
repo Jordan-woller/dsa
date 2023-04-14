92cf9aa150efa3204692d77d117cf5fdacd9b0d7
Jake D <113384088+JakeDansereau@users.noreply.github.com>
2023-04-13 22:21:06 -0400

    Pseudocode

--- main.cpp
+++ main.cpp
@@ -11,6 +11,19 @@ int main(){
     std::cout << "Time taken by function: "
         << duration.count() << " microseconds" << std::endl;
 
+
+    //main options menu
+    //Ask the user what they want to do
+        //a. insert a song of your choice into the list
+        //b. sort two playlists together
+            //what do you want to do?
+            //a. Make a playlist from scratch
+            //b. Choose from a preselected playlist
+        //c.
+
+
+
     return 0;
+
 }
 
